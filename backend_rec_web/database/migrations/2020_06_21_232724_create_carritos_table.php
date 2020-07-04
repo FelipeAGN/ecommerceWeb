f74c97d70c2f05bc272b84a27f8add7af1c50117
felipe <felipe.agn98@gmail.com>
2020-07-04 12:32:16 -0400

    cambio en modelo, referencia de carrito a id user como 'rut'

--- backend_rec_web/database/migrations/2020_06_21_232724_create_carritos_table.php
+++ backend_rec_web/database/migrations/2020_06_21_232724_create_carritos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarritosTable extends Migration
+class oioCreateCarritosTable extends Migration
 {
     /**
      * Run the migrations.
